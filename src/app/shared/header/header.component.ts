255eddcfbb7f3874be51fc81d3257fd5b7fe06d9
Markywa <mark0_baranov@mail.ru>
2026-06-02 18:09:26 +0700

    fix: правки кода

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -195,7 +195,7 @@ export class HeaderComponent {
 
   sendQuestionModal(mobile?: boolean): void {
     const dialogus = this.dialog.open(QuestionModalComponent, {
-      height: mobile ? 'auto' : '495px',
+      height: mobile ? 'auto' : '510px',
       width: mobile ? 'auto' : '550px',
     })
 
